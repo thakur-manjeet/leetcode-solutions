b6aaebbc8f0b1abd90998e49e32e63e28fb12749
Manjeet_Thakur <manjeetthakur.com243@gmail.com>
2026-09-24 22:28:18 +0530

    Time: 4 ms (76.53%), Space: 19.9 MB (87.2%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,47 +1,44 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.cap = k
-        self.arr = [None] * self.cap
-        self.f = 0
-        self.r = -1
-        self.curr_size = 0
+        self.cap=k
+        self.arr=[None]*k
+        self.f=0
+        self.r=-1
+        self.size=0
+        
 
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
-        
-        # Circular increment of rear pointer
-        self.r = (self.r + 1) % self.cap
-        self.arr[self.r] = value
-        self.curr_size += 1
-        return True
+
+        self.r= (self.r +1)%self.cap
+        self.arr[self.r]=value
+        self.size+=1
+        return True    
 
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
-        
-        # Circular increment of front pointer
-        self.f = (self.f + 1) % self.cap
-        self.curr_size -= 1
-        return True
+        self.f=(self.f +1)%self.cap
+        self.size-=1
+        return True    
 
     def Front(self) -> int:
         if self.isEmpty():
             return -1
-        return self.arr[self.f]
+        return self.arr[self.f]    
 
     def Rear(self) -> int:
         if self.isEmpty():
             return -1
-        return self.arr[self.r]
+        return self.arr[self.r]   
 
     def isEmpty(self) -> bool:
-        return self.curr_size == 0
+        return self.size == 0
 
     def isFull(self) -> bool:
-        return self.curr_size == self.cap
-    
+        return self.size == self.cap
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
